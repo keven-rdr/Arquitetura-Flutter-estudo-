bee63eb63a1eac3412e9a519fd4a75efb23ece25
keven rodrigues <kevenlucas2015@gmail.com>
2025-11-18 13:54:35 -0300

    - Refactor function the build

--- lib/scenes/Favorites/favorites_scene.dart
+++ lib/scenes/Favorites/favorites_scene.dart
@@ -1,12 +1,40 @@
+import 'package:arqmvvm/resources/shared/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
-class FavoritesPage extends StatelessWidget {
+class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
+
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Favorites Page', style: TextStyle(color: Colors.white)));
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: dividerColorLight,
+          body: SafeArea(child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text("Favorites Page"),
+                ],
+              )
+          )),
+        )
+      ],
+    );
   }
 }
+
+
+
--- lib/scenes/Matches/match_scene.dart
+++ lib/scenes/Matches/match_scene.dart
@@ -1,10 +1,37 @@
+import 'package:arqmvvm/resources/shared/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MatchesPage extends StatelessWidget {
+class MatchesPage extends StatefulWidget {
   const MatchesPage({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('Matches Page', style: TextStyle(color: Colors.white)));
-  }
+  State<MatchesPage> createState() => _MatchesPageState();
 }
+
+ class _MatchesPageState extends State<MatchesPage> {
+
+   @override
+   void initState() {
+     super.initState();
+   }
+
+   @override
+   Widget build(BuildContext context) {
+     return Stack(
+       children: [
+         Scaffold(
+           backgroundColor: dividerColorLight,
+           body: SafeArea(child: SingleChildScrollView(
+               padding: const EdgeInsets.all(16.0),
+               child: Column(
+                 children: [
+                   Text("Matches Page"),
+                 ],
+               )
+           )),
+         )
+       ],
+     );
+   }
+ }
