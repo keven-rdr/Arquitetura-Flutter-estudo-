1fdf18d43b31d0151b20ac9087b65f4a04f2e584
keven rodrigues <kevenlucas2015@gmail.com>
2025-11-27 17:08:02 -0300

    - Ajustando o coordinator para adicionar logica para voltar

--- lib/resources/shared/app_coordinator.dart
+++ lib/resources/shared/app_coordinator.dart
@@ -1,10 +1,13 @@
 import 'package:arqmvvm/scenes/main_tab/main_tab_factory.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../scenes/Matches/matches_factory.dart';
 import '../../scenes/login/login_factory.dart';
 
 class AppCoordinator {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  final ValueNotifier<int> tabNotifier = ValueNotifier<int>(0);
 
   Widget startApp() {
     return goToLoginWidget();
@@ -17,17 +20,19 @@ class AppCoordinator {
   void goToLogin() {
     final login = LoginFactory.make(coordinator: this);
 
-    // Limpa toda a pilha de navegação (Home, Profile, etc.) e coloca o Login
     navigatorKey.currentState?.pushAndRemoveUntil(
       MaterialPageRoute(builder: (_) => login),
-          (Route<dynamic> route) => false, // 'false' garante que todas as rotas anteriores sejam removidas
+          (Route<dynamic> route) => false,
     );
   }
-
 
   void goToHome() {
     final home = MainTabFactory.make(coordinator: this);
     navigatorKey.currentState?.pushReplacement( MaterialPageRoute(builder: (_) => home ));
+  }
+
+  void selectTab(int index) {
+    tabNotifier.value = index;
   }
 
 }
